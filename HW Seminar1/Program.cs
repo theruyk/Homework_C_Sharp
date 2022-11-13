247b6d06cdd7e6302f6c2ce6b8ee79849daa5bec
Иван Соколов <vano95.cokol@gmail.com>
2022-11-13 20:23:32 +0600

    1 task

--- HW Seminar1/Program.cs
+++ HW Seminar1/Program.cs
@@ -1,5 +1,16 @@
 ﻿/* Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.*/
-
+Console.WriteLine("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("number1");
+}
+else 
+{
+    Console.WriteLine("number2");
+}
 /* Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.*/
 
 /* Напишите программу, которая принимает на вход число и выдает, является ли число четным. */
