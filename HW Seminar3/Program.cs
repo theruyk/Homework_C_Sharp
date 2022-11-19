0bb60a373052762b1a6b73f450aa74fc78b11b7b
Иван Соколов <vano95.cokol@gmail.com>
2022-11-19 21:22:13 +0600

    решил 1 задачу

---
+++ HW Seminar3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+/*
+void Palindrom(int number)
+{
+    int number1 = number;
+    int Reverse = 0;
+    while (number != 0){
+        int end = number % 10;
+        Reverse = (Reverse * 10) + end;
+        number = number / 10;
+    }
+
+    if (number1 == Reverse && Reverse !=0 ){
+        Console.WriteLine("Yes");
+    }
+    else {Console.WriteLine("No");}
+
+}
+
+Console.WriteLine("Input your number ");
+int num = Convert.ToInt32(Console.ReadLine());
+Palindrom(num);
+*/
